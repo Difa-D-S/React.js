b3f658c68626b41286f9922fac7254d510ea61c3
Difa-D-S <difads123@gmail.com>
2024-03-04 10:13:32 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import Card from 'react-bootstrap/Card';
+import bird from './assets/download.jpeg';
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from './component/ExampleCarouselImage';
+
 
 const App = () => {
   const [todo, setTodo] = useState({
@@ -30,6 +35,51 @@ const App = () => {
     ))}
     </div>
     </div>
+
+  <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={bird} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Carousel>
+      <Carousel.Item>
+        <ExampleCarouselImage text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <ExampleCarouselImage text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+
+
+{/* State and Use State */}
+
+        
+    
     </>
   );
 }
---
+++ src/component/ExampleCarouselImage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import bird from '../assets/download.jpeg';
+// import travel from '../assets/Travel2.png';
+const ExampleCarouselImage = () => {
+  return (
+    <>
+    <img src={bird} alt="bird" />
+    </>
+  )
+}
+
+export default ExampleCarouselImage
