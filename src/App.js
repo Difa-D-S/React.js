9cc08f23ea72890ddf17f682539abbba21431596
Difa-D-S <difads123@gmail.com>
2024-05-10 09:54:14 +0530

    Todo CRUD

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import React from 'react';
-import './App.css';
-import Addtext from './component/Addtext';
-import Buttoncomponent from './component/Buttoncomponent';
-import TodoList from './component/TodoList';
-// import EditDeleteBtn from './component/EditDeleteBtn';
-// import Button from 'react-bootstrap/Button'
+import React from "react";
+import "./App.css";
+import Addtext from "./component/Addtext";
 
 const App = () => {
-
   return (
     <>
-      <div className='list'>
-        <div className='flex'> 
-          <Addtext />
-
-          <Buttoncomponent />
-
-        </div>   
-          <TodoList />
-      </div>
+      <Addtext />
     </>
   );
-}
-  
+};
 
 export default App;
-
-// const todoReducer = copy the code from w3
-
-
-//const [todo, dispatch] = useReducer(todoReducer, initialvalue);
-
-// const action= {
-//   type:'',
-//   payload: pass panura id ah kudukanum
-// }
--- src/component/Addtext.jsx
+++ src/component/Addtext.jsx
@@ -1,26 +1,65 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import {context} from '../Context'
+import React, { useState } from 'react'
 
 const Addtext = () => {
 
-  const values = useContext(context)
+  const [todo, setTodo] = useState([]);
+  const date = new Date();
+  const [inputValue, setInputValue] = useState("");
+  const [inputDesc, setInputDesc] = useState("");
 
-  const onkeyDown = (event) => {
-    if(event.key === "Enter") {
-      values.addTodoHandler();
-    }
-  }
+  const addHandler = () => {
+    const arrtodo = {
+      id: Math.random() * 20,
+      title: inputValue,
+      description: inputDesc,
+      createdDate: `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`,
+    };
+    setTodo([...todo, arrtodo]);
+    setInputValue("");
+    setInputDesc("");
+  };
 
-  const addTodoInput = useRef(null);
-
-  useEffect(()=>{
-    addTodoInput.current.focus();
-  })
-
+  const handleRemoveTask = (taskId) => {
+    const updatedTasks = todo.filter((task) => task.id !== taskId);
+    setTodo(updatedTasks);
+  };
   return (
     <>
-        <input className="todo-input" type="text" placeholder="Enter your todo" value = {values.inputValue}
-         onChange={(e) => values.setInputValue(e.target.value)} onKeyDown={onkeyDown} ref={addTodoInput}></input>
+      <div className='list-add'>
+      <div className='list'>
+        <input type="text" className="w-100 p-1" value={inputValue} placeholder='Title'
+          onChange={(e) => { setInputValue(e.target.value) }} />
+
+        <input  className="w-100 my-3 p-1" style={{textAlign: "justify"}}
+          type="text" value={inputDesc} placeholder='Description'
+          onChange={(e) => { setInputDesc(e.target.value) }} />
+
+        <button className="bg-primary p-2 w-100 text-white border rounded-lg"
+          onClick={addHandler} > ADD </button>
+
+      {todo.map((task) => (
+          <li key={task.id} style={{listStyle: 'none', marginTop:"10px", fontWeight: "bold"}}>
+            
+            {task.title}
+           
+            <p>{task.description}</p>
+            
+            <span>{task.createdDate}</span>
+            
+              <span className='d-flex'>
+                <button className="bg-primary p-2 w-100 text-white me-3">
+                    edit
+                </button>
+
+                <button onClick={() => handleRemoveTask(task.id)} className="bg-danger p-2 w-100 text-white">
+                    delete
+                </button>
+                <hr/>
+              </span>
+          </li>
+        ))}
+      </div>
+      </div>
     </>
   )
 }
